9a1a2e0a7319bd8eaa77a143e7ad404efc909d86
Serhii Chumak <s4umak@gmail.com>
2025-12-05 16:04:36 +0200

    Modified with flake8 urls, serializer in py modelveiwset practice

--- author/views.py
+++ author/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from  author.models import Author
-from .serializers import AuthorSerializer
+from author.models import Author
+from author.serializers import AuthorSerializer
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
