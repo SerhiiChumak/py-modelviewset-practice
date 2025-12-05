42de46e0db4b1330f7f2c99c256f1793ac021fab
Serhii Chumak <s4umak@gmail.com>
2025-12-05 14:53:59 +0200

    Modified urls.py pu modelveiwset practice

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,20 @@
-# Create your urls here
+from django.urls import path
+from author.views import AuthorViewSet
 
-urlpatterns = []
+app_name = "author"
+
+author_list = AuthorViewSet.as_view(actions={"get": "list", "post": "create"})
+
+author_detail = AuthorViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
+
+urlpatterns = [
+    path("authors/", author_list, name="author_list"),
+    path("authors/<int:pk>/", author_detail, name="author_detail")
+]
